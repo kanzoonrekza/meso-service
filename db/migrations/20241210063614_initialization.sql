77e903068e49b6fc4c6eed94af69b4427c42ef9b
Kanzoon Rekza <kanzoonrkz@gmail.com>
2024-12-13 14:23:06 +0700

    handle null values from db

--- db/migrations/20241210063614_initialization.sql
+++ db/migrations/20241210063614_initialization.sql
@@ -4,14 +4,14 @@ CREATE TYPE status AS ENUM ('to do', 'in progress', 'done');
 
 CREATE TABLE IF NOT EXISTS lists (
     id BIGSERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
+    title TEXT NOT NULL CHECK (length(trim(title)) > 0),
     description TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS tasks (
     id BIGSERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
+    title TEXT NOT NULL CHECK (length(trim(title)) > 0),
     description TEXT,
     status status NOT NULL DEFAULT 'to do',
     parent_list_id BIGINT REFERENCES lists(id),
